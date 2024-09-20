a24ffa15b462b88577d920a5b36d893961f9a3f8
nina <nina.e@systemli.org>
2024-09-20 10:01:53 +0200

    added chat id logging and emojis to telegram

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
 const { loadEvents } = require("./drivers/events.js");
 require('dotenv').config()
 
-process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
-});
+
   
 console.log("Alle Ereignisse")
 loadEvents().forEach(element => {
